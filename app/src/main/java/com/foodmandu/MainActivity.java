c094fb35a4d1401e0b8601157631089d14c9dd68
NimeshPdl <nimeshpaudel1025@gmail.com>
2020-01-03 22:49:27 +0545

    Navigations nearly completed with signupandlogin completed!!!

--- app/src/main/java/com/foodmandu/MainActivity.java
+++ app/src/main/java/com/foodmandu/MainActivity.java
@@ -2,9 +2,22 @@ package com.foodmandu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.foodmandu.bll.LoginBLL;
+import com.foodmandu.strictmode.StrictModeClass;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private EditText etEmail, etPassword;
+    private TextView click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +25,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        click = findViewById(R.id.click);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login(); }
+        });
     }
 
+    private void login() {
+        String username = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        LoginBLL loginBLL = new LoginBLL();
+
+        StrictModeClass.StrictMode();
+        if (loginBLL.checkUser(username, password)) {
+            Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            Toast.makeText(this, "Either username or password is incorrect", Toast.LENGTH_SHORT).show();
+            etEmail.requestFocus();
+        }
+    }
 }
